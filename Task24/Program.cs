c313fbe07d0e0443ded3846c71ec2cb873df6a3c
Veritas97 <lykovcl@gmail.com>
2022-11-28 16:28:28 +0900

    decided task24

---
+++ Task24/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+
+    }
+    return sum;
+}
+
+int sum = SumDigits(numA);
+
+Console.Write($"Ваша сумма всех цифр - {sum} ");
