ad6258b8691578c625bf073160b0a66a99d5f262
Veritas97 <lykovcl@gmail.com>
2022-11-28 17:12:07 +0900

    decided task26

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+if (numberN < 0) numberN = -numberN;
+{
+    int sumDigits = SumNumber(numberN);
+    Console.WriteLine(sumDigits);
+}
