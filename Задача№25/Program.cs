f15ea3a39fb7701671f5d092d15e14bb1e3944af
Veritas97 <lykovcl@gmail.com>
2022-11-28 16:37:04 +0900

    decided task25

--- Задача№25/Program.cs
+++ Задача№25/Program.cs
@@ -3,7 +3,7 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-int SumDigits(int num1, int num2)
+int SumEXPO(int num1, int num2)
 {
     int sum = 1;
     for (int i = 1; i <= num2; i++)
@@ -14,6 +14,6 @@ int SumDigits(int num1, int num2)
     return sum;
 }
 
-int sum = SumDigits(numberA, numberB);
+int sum = SumEXPO(numberA, numberB);
 
 Console.Write($"Натуральное число в кубе - {sum} ");
