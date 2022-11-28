e665468aa2d045c8113570430f5a10364a781f4f
Veritas97 <lykovcl@gmail.com>
2022-11-28 18:01:10 +0900

    decided task30

---
+++ Task30/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0,2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+RandomArray(array);
+PrintArray(array);
