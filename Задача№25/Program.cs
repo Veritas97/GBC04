95353617968610d1d92b981deafd1dd81b0e5cd7
Veritas97 <lykovcl@gmail.com>
2022-11-28 16:36:09 +0900

    decided task25

---
+++ Задача№25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num1, int num2)
+{
+    int sum = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        sum = sum * num1;
+
+    }
+    return sum;
+}
+
+int sum = SumDigits(numberA, numberB);
+
+Console.Write($"Натуральное число в кубе - {sum} ");
